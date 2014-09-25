624901c6e833c2e88dbdd2cff4fe02f758c51717
Erik Belusic <erikbelusic@eriks-air.home>
2014-09-25 00:20:39 -0400

    basic clouds responding to mouse

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
+/*
+ Defining our variables
+ world and viewport are DOM elements,
+ worldXAngle and worldYAngle are floats that hold the world rotations,
+ d is an int that defines the distance of the world from the camera
+ */
+var world = document.getElementById( 'world' ),
+    viewport = document.getElementById( 'viewport' ),
+    worldXAngle = 0,
+    worldYAngle = 0,
+    d = -500;
 
+/*
+ Event listener to transform mouse position into angles
+ from -180 to 180 degress, both vertically and horizontally
+ */
+if (window.DeviceOrientationEvent) {
+    console.log("DeviceOrientation is supported");
+}
+window.addEventListener( 'mousemove', function( e ) {
+    worldYAngle = -( .5 - ( e.clientX / window.innerWidth ) ) * 4;
+    worldXAngle = ( .5 - ( e.clientY / window.innerHeight ) ) * 4;
+    updateView();
+} );
+
+/*
+ Changes the transform property of world to be
+ translated in the Z axis by d pixels,
+ rotated in the X axis by worldXAngle degrees and
+ rotated in the Y axis by worldYAngle degrees.
+ */
+function updateView() {
+    world.style.transform = 'translateZ( ' + d + 'px ) \
+        rotateX( ' + worldXAngle + 'deg) \
+        rotateY( ' + worldYAngle + 'deg)';
+}
